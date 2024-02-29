f59cf5e99827f3ee0982b5119a210cca7d33920c
thecybercroc <thecybercroc@gmail.com>
2024-02-29 12:10:04 +0530

    Before 29 Feb 2k24

---
+++ about.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A placeat necessitatibus quam, temporibus qui odit hic tempore veniam saepe atque fugiat, voluptas nulla quidem est! Repellat eveniet reiciendis nulla dolores temporibus officia corrupti fuga delectus obcaecati deserunt autem, aperiam fugit, adipisci saepe amet corporis debitis veniam, doloremque sunt. Autem nobis consequuntur maiores, architecto nemo cum expedita at doloremque eum. Ex quibusdam ipsam ut incidunt!";
+  
+//     let index = 0;
+//     const textElement = document.getElementById('typewriter-text');
+  
+//     function typeWriter() {
+//       if (index < text.length) {
+//         textElement.textContent += text.charAt(index);
+//         index++;
+//         setTimeout(typeWriter, 10); // Adjust the typing speed (milliseconds)
+//       }
+//     }
+  
+//     typeWriter();
+// });
